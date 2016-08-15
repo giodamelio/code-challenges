2e706806b50ec60de9a54f6093c1a4edc562f0c5
Gio d'Amelio <giodamelio@gmail.com>
2016-08-15 12:14:24 -0700

    Project Euler: Clojure: refactor problem 1

--- project_euler/clojure/src/project_euler/problems/p0001.clj
+++ project_euler/clojure/src/project_euler/problems/p0001.clj
@@ -1,8 +1,9 @@
 (ns project-euler.problems.p0001)
 
-(defn seq-divisible-by-3-or-5
-  "Returns a lazy sequence of evey natural number evenly divisible by 3 or 5"
-  []
-  (filter #(or (= (rem % 3) 0) (= (rem % 5) 0)) (range)))
+(defn seq-divisible-by
+  "Returns a lazy sequence of evey natural number evenly divisible by one or more numbers"
+  [& nums]
+  (letfn [(divisible-by [n d] (zero? (rem n d)))]
+    (filter #(some (partial divisible-by %) nums) (range))))
 
-(def answer (reduce + (take-while #(< % 1000) (seq-divisible-by-3-or-5))))
+(def answer (reduce + (take-while #(< % 1000) (seq-divisible-by 3 5))))
