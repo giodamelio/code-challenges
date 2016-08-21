a55e64adba23d3097f23b5a957284a3cb6d9843d
Gio d'Amelio <giodamelio@gmail.com>
2016-08-21 15:31:47 -0700

    Project Euler: Clojure: change main to run a problem by number

--- project_euler/clojure/src/project_euler/core.clj
+++ project_euler/clojure/src/project_euler/core.clj
@@ -1,8 +1,17 @@
 (ns project-euler.core
-  (:require [project-euler.problems.p0001 :as p0001])
   (:gen-class))
 
+(defn run-problem
+  "Run a specific problem"
+  [n]
+  (let [number (format "%04d" n)
+        problem-ns (str "project-euler.problems.p" number)]
+    (require (symbol problem-ns))
+    (load-string (str problem-ns "/answer"))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Run a problem"
   [& args]
-  (println args))
+  (if (and args (= 1 (count args)))
+    (println (time (run-problem (Integer/parseInt (first args)))))
+    (println "Usage: lein run [number]")))
