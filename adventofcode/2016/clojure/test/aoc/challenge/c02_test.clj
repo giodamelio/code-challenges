bffd7edab9f0e849140e609681183291056df400
Gio d'Amelio <giodamelio@gmail.com>
2016-12-04 22:46:25 -0800

    AOC: Clojure: complete day 2 part 2

--- adventofcode/2016/clojure/test/aoc/challenge/c02_test.clj
+++ adventofcode/2016/clojure/test/aoc/challenge/c02_test.clj
@@ -2,22 +2,39 @@
   (:require [clojure.test :refer :all]
             [aoc.challenge.c02 :refer :all]))
 
-(deftest go-test
+(deftest go-normal-test
   (testing "go up"
-    (is (= (go 5 :U) 2))
-    (is (= (go 3 :U) 3)))
+    (is (= (go-normal 5 :U) 2))
+    (is (= (go-normal 3 :U) 3)))
 
   (testing "go down"
-    (is (= (go 5 :D) 8))
-    (is (= (go 7 :D) 7)))
+    (is (= (go-normal 5 :D) 8))
+    (is (= (go-normal 7 :D) 7)))
 
   (testing "go left"
-    (is (= (go 5 :L) 4))
-    (is (= (go 4 :L) 4)))
+    (is (= (go-normal 5 :L) 4))
+    (is (= (go-normal 4 :L) 4)))
 
   (testing "go right"
-    (is (= (go 5 :R) 6))
-    (is (= (go 3 :R) 3))))
+    (is (= (go-normal 5 :R) 6))
+    (is (= (go-normal 3 :R) 3))))
+
+(deftest go-weird-test
+  (testing "go up"
+    (is (= (go-weird 5 :U) 5))
+    (is (= (go-weird 3 :U) 1)))
+
+  (testing "go down"
+    (is (= (go-weird 5 :D) 5))
+    (is (= (go-weird 7 :D) "B")))
+
+  (testing "go left"
+    (is (= (go-weird 5 :L) 5))
+    (is (= (go-weird 4 :L) 3)))
+
+  (testing "go right"
+    (is (= (go-weird 5 :R) 6))
+    (is (= (go-weird 3 :R) 4))))
 
 (deftest parse-paths-text
   (testing "parse a few paths"
@@ -28,12 +45,12 @@
 
 (deftest walk-path-test
   (testing "walk some a path"
-    (is (= (walk-path 5 [:U :L :L]) 1))
-    (is (= (walk-path 1 [:R :R :D :D :D]) 9))
-    (is (= (walk-path 9 [:L :U :R :D :L]) 8))
-    (is (= (walk-path 8 [:U :U :U :U :D]) 5))))
+    (is (= (walk-path 5 [:U :L :L] go-normal) 1))
+    (is (= (walk-path 1 [:R :R :D :D :D] go-normal) 9))
+    (is (= (walk-path 9 [:L :U :R :D :L] go-normal) 8))
+    (is (= (walk-path 8 [:U :U :U :U :D] go-normal) 5))))
 
 (deftest walk-paths-test
   (testing "walk a series of paths"
-    (is (= (walk-paths 5 '([:U :L :L] [:R :R :D :D :D] [:L :U :R :D :L] [:U :U :U :U :D]))
+    (is (= (walk-paths 5 '([:U :L :L] [:R :R :D :D :D] [:L :U :R :D :L] [:U :U :U :U :D]) go-normal)
            '(1 9 8 5)))))
