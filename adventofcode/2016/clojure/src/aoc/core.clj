2f4287533cc71034084533b87990426a8fe40d07
Gio d'Amelio <giodamelio@gmail.com>
2016-12-04 22:49:32 -0800

    AOC: Clojure: make 'lein run' run both first and second part

--- adventofcode/2016/clojure/src/aoc/core.clj
+++ adventofcode/2016/clojure/src/aoc/core.clj
@@ -8,7 +8,8 @@
         problem-ns (str "aoc.challenge.c" number)]
     (try
       (require (symbol problem-ns))
-      (load-string (str "(" problem-ns "/answer)"))
+      (println (str "Part 1: " (load-string (str "(" problem-ns "/answer-part-1)"))))
+      (println (str "Part 2: " (load-string (str "(" problem-ns "/answer-part-2)"))))
       (catch java.io.FileNotFoundException e
         (str "Problem #" number " does not exist")))))
 
