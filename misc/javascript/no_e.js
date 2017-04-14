ff747bc6de677bacefe661112ce26bce9eb97398
Gio d'Amelio <giodamelio@gmail.com>
2017-04-14 16:00:14 -0700

    Misc: Jacascript: update no_e.js to look for uppercase E's

--- misc/javascript/no_e.js
+++ misc/javascript/no_e.js
@@ -2,13 +2,14 @@ const assert = require('assert');
 
 // No e's in this funciton
 const no_101 = (string) => (
-  that_char = (![] + '')[4],
-  !string.split('').find((char) => char === that_char)
+  that_char = (![] + '')[4], that_tall_char = (/a/.constructor + '')[12],
+  !string.split('').find((char) => char === that_char || char === that_tall_char)
 )
 
 // Test our function
 assert( no_101('abcdfghijklmnopqrstuvxyz'));
 assert(!no_101('eeeeeeee'));
+assert(!no_101('EEEEEEEE'));
 assert(!no_101('eek!'));
 assert( no_101('ееk!')); // What!
 console.log('Success!');
