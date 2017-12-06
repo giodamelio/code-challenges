16bcc4acbbf67116f2c9e3e30b432ab962aca03b
Gio d'Amelio <giodamelio@gmail.com>
2017-12-06 15:19:23 -0800

    AOC: Elixir: create debug macro for quick debug logging

--- adventofcode/2017/elixir/lib/puzzles/utils.ex
+++ adventofcode/2017/elixir/lib/puzzles/utils.ex
@@ -12,4 +12,37 @@ defmodule AOC.Puzzles.Utils do
     {head, tail} = Enum.split(list, n)
     tail ++ head
   end
+
+  # A macro for easy debug logging
+
+  # Debug a list of variables
+  defmacro debug(variables) when is_list(variables) do
+    quote bind_quoted: [
+      names: variables |> Enum.map(&variable_name/1),
+      values: variables
+    ] do
+      for {name, value} <- Enum.zip(names, values) do
+        print_variable(name, value)
+      end
+      :ok
+    end
+  end
+
+  # Debug a single variable
+  defmacro debug(variable) do
+    quote bind_quoted: [variable_name: variable_name(variable),
+                        variable_text: variable] do
+      print_variable(variable_name, variable_text)
+    end
+  end
+
+  # Print out a single variable
+  def print_variable(variable_name, variable_text) do
+    IO.puts("#{variable_name}: #{inspect variable_text}")
+  end
+
+  # Get the name of a variable as a string
+  def variable_name(variable) do
+    Atom.to_string(elem(variable, 0))
+  end
 end
--- adventofcode/2017/elixir/test/puzzles/utils_test.exs
+++ adventofcode/2017/elixir/test/puzzles/utils_test.exs
@@ -2,6 +2,9 @@ defmodule AOCTest.Puzzles.UtilsTest do
   use ExUnit.Case
 
   import AOC.Puzzles.Utils
+  import ExUnit.CaptureIO
+
+  require AOC.Puzzles.Utils
 
   describe "rotate/2" do
     test "rotates right by default" do
@@ -16,6 +19,41 @@ defmodule AOCTest.Puzzles.UtilsTest do
 
     test "rotate 2 left" do
       assert rotate([1, 2, 3, 4, 5, 6], 2, :LEFT) == [3, 4, 5, 6, 1, 2]
+    end
+  end
+
+  describe "debug/1" do
+    test "print a variable" do
+      hello = "world"
+      assert capture_io(fn () -> debug(hello) end) ==
+        ~s(hello: "world"\n)
+    end
+
+    test "print a list" do
+      numbers = [1, 2, 3, 4]
+      assert capture_io(fn () -> debug(numbers) end) ==
+        ~s(numbers: [1, 2, 3, 4]\n)
+    end
+
+    test "debug multiple variables" do
+      numbers = [1, 2, 3, 4]
+      letters = ["a", "b", "c", "d"]
+
+      assert capture_io(fn () -> debug([numbers, letters]) end) ==
+        ~s(numbers: [1, 2, 3, 4]\nletters: ["a", "b", "c", "d"]\n)
+    end
+  end
+
+  describe "print_variable/1" do
+    test "print a name and variable" do
+      assert capture_io(fn () -> print_variable("haha", [1, 2, 3]) end) ==
+        ~s(haha: [1, 2, 3]\n)
+    end
+  end
+
+  describe "variable_name/1" do
+    test "get the name of a variable" do
+      assert variable_name({:hello, [], nil}) == "hello"
     end
   end
 end
