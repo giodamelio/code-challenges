be8292ead7042cd33edab61c969a9da029b1c7b6
Gio d'Amelio <giodamelio@gmail.com>
2017-12-08 00:36:19 -0800

    AOC: Elixir: complete day 8

---
+++ adventofcode/2017/elixir/lib/puzzles/n08.ex
@@ -0,0 +1,120 @@
+defmodule AOC.Puzzles.N08 do
+  def run(input) do
+    {:ok, registry} = Registers.start_link([])
+
+    input
+    |> Enum.map(fn (op) ->
+      should_run = Registers.op(registry, :COMPARE, {op.comparison_register,
+                                                     op.comparison_operator,
+                                                     op.comparison_value})
+      if should_run do
+        Registers.op(registry, op.operation, {op.register,
+                                              op.amount})
+      end
+    end)
+
+    registry
+  end
+
+  def first_half(input) do
+    registry = run(input)
+
+    {_register, value} = Registers.dump(registry)
+    |> Enum.max_by(&elem(&1, 1))
+
+    value
+  end
+
+  def second_half(input) do
+    registry = run(input)
+
+    Registers.max(registry)
+  end
+end
+
+defmodule Registers do
+  use GenServer
+
+  #### Client API ####
+
+  # Start a set of registers
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  # Run a operation
+  def op(server, name, data) do
+    GenServer.call(server, {:OP, name, data})
+  end
+
+  # Dump the contents of all the registers
+  def dump(server) do
+    GenServer.call(server, :DUMP)
+  end
+
+  # Get the largest a register has ever been
+  def max(server) do
+    GenServer.call(server, :MAX)
+  end
+
+  #### Helpers ####
+
+  # Put a value to a map and track the max value
+  def put_and_track_max(map, key, value, max) do
+    {Map.put(map, key, value),
+    (if value > max, do: value, else: max)}
+  end
+
+  #### Server Callbacks ####
+
+  # Start with an empty Map of registers and a max value of 0
+  def init(:ok) do
+    {:ok, {%{}, 0}}
+  end
+
+  # Dump all the registers
+  def handle_call(:DUMP, _from, {registers, max}) do
+    {:reply, registers, {registers, max}}
+  end
+
+  # Return the largest a register has ever been
+  def handle_call(:MAX, _from, {registers, max}) do
+    {:reply, max, {registers, max}}
+  end
+
+  # Increment a register
+  def handle_call({:OP, :INC, {register, amount}}, _from, {registers, max}) do
+    value = Map.get(registers, register, 0)
+    {new_registers, new_max} =
+      put_and_track_max(registers, register, value + amount, max)
+
+    {:reply, :ok, {new_registers, new_max}}
+  end
+
+  # Decrement a register
+  def handle_call({:OP, :DEC, {register, amount}}, _from, {registers, max}) do
+    value = Map.get(registers, register, 0)
+    {new_registers, new_max} =
+      put_and_track_max(registers, register, value - amount, max)
+
+    {:reply, :ok, {new_registers, new_max}}
+  end
+
+  # Compare a register to a value
+  def handle_call({:OP,
+                   :COMPARE,
+                   {register, operator, value}}, _from, {registers, max}) do
+    register_value = Map.get(registers, register, 0)
+    operator = case operator do
+                :EQUAL            -> :==
+                :NOT_EQUAL        -> :!=
+                :GREATER_THEN     -> :>
+                :LESS_THEN        -> :<
+                :GREATER_OR_EQUAL -> :>=
+                :LESS_OR_EQUAL    -> :<=
+              end
+    {:reply,
+     apply(Kernel, operator, [register_value, value]),
+     {registers, max}}
+  end
+end
---
+++ adventofcode/2017/elixir/test/puzzles/n08_test.exs
@@ -0,0 +1,97 @@
+defmodule AOCTest.Puzzles.N08Test do
+  use ExUnit.Case
+
+  import AOC.Puzzles.N08
+
+  @regex ~r/(?<register>\w+)\ (?<operation>(?:inc|dec))\ (?<amount>-?\d+)\ if\ (?<comparison_register>\w+)\ (?<comparison_operator>(?:==|!=|<|>|<=|>=))\ (?<comparison_value>-?\d+)/
+
+  # Read the puzzle input from a file and parse it
+  setup_all do
+    {:ok, raw} = File.read("test/puzzles/inputs/08.txt")
+
+    input = raw
+    # Trim surronding whitespace
+    |> String.trim
+    # Split by line
+    |> String.split("\n")
+    # Parse each line with regex
+    |> Enum.map(&Regex.named_captures(@regex, &1))
+    # Convert the map keys to atoms
+    |> Enum.map(fn (program) ->
+      Enum.reduce(program, %{}, fn ({key, val}, acc) ->
+        Map.put(acc, String.to_atom(key), val)
+      end)
+    end)
+    # Parse some of the values further
+    |> Enum.map(fn (line) ->
+      line
+      # Convert the operations to atoms
+      |> Map.update!(:operation, fn (op) ->
+        op |> String.upcase |> String.to_atom
+      end)
+      # Parse some numbers
+      |> Map.update!(:amount, &String.to_integer/1)
+      |> Map.update!(:comparison_value, &String.to_integer/1)
+      # Convert comparison operator to atom
+      |> Map.update!(:comparison_operator, fn (operator) ->
+        case operator do
+          "==" -> :EQUAL
+          "!=" -> :NOT_EQUAL
+          ">"  -> :GREATER_THEN
+          "<"  -> :LESS_THEN
+          ">=" -> :GREATER_OR_EQUAL
+          "<=" -> :LESS_OR_EQUAL
+        end
+      end)
+    end)
+
+    # Example to be shared between parts one and two
+    example = [
+      %{:amount => 5,
+        :comparison_operator => :GREATER_THEN,
+        :comparison_register => "a",
+        :comparison_value => 1,
+        :operation => :INC,
+        :register => "b"},
+      %{:amount => 1,
+        :comparison_operator => :LESS_THEN,
+        :comparison_register => "b",
+        :comparison_value => 5,
+        :operation => :INC,
+        :register => "a"},
+      %{:amount => -10,
+        :comparison_operator => :GREATER_OR_EQUAL,
+        :comparison_register => "a",
+        :comparison_value => 1,
+        :operation => :DEC,
+        :register => "c"},
+      %{:amount => -20,
+        :comparison_operator => :EQUAL,
+        :comparison_register => "c",
+        :comparison_value => 10,
+        :operation => :INC,
+        :register => "c"}]
+
+    {:ok, input: input, example: example}
+  end
+
+  describe "Part 1" do
+    test "First example", %{:example => example} do
+      assert first_half(example) == 1
+    end
+
+    test "solution", %{:input => input} do
+      assert first_half(input) == 3612
+    end
+  end
+
+  describe "Part 2" do
+    test "First example", %{:example => example} do
+      assert second_half(example) == 10
+    end
+
+    test "solution", %{:input => input} do
+      assert second_half(input) == 3818
+    end
+  end
+end
