d18e2b506379a1590830eee11bafe16f220dedf8
Gio d'Amelio <giodamelio@gmail.com>
2017-12-09 17:56:33 -0800

    AOC: Elixir: day 9

--- adventofcode/2017/elixir/lib/puzzles/n09.ex
+++ adventofcode/2017/elixir/lib/puzzles/n09.ex
@@ -1,53 +1,37 @@
 defmodule AOC.Puzzles.N09 do
-  import AOC.Puzzles.Utils
-  require AOC.Puzzles.Utils
-
-  # These next few functions delete some things from a string
-  def delete_canceled(input) do
-    Regex.replace(~r/\!./, input, "")
+  ## Parse the normal non-garbage sections
+  # Default values
+  def parse(string), do: parse(string, 1, 0)
+  # When we hit a `{` increment the depth by one and keep the count
+  def parse("{" <> rest, depth, count) do
+    {child_depth, count} = parse(rest, depth + 1, count)
+    {depth + child_depth, count}
   end
+  # When we hit a `}` increase the level by one
+  def parse("}" <> rest, depth, count), do: parse(rest, depth - 1, count)
+  # When we hit a `<` we are starting a garbage section. See below
+  def parse("<" <> rest, depth, count), do: garbage(rest, depth, count)
+  # When we hit a `,` ignore it and continue on
+  def parse("," <> rest, depth, count), do: parse(rest, depth, count)
+  # When we hit the end of the string, send 0 so we can unwind
+  def parse("", depth, count), do: {0, count}
 
-  def delete_garbage(input) do
-    Regex.replace(~r/\<.+?\>/, input, "")
-  end
-
-  def delete_extra_commas(input) do
-    Regex.replace(~r/,(?=,|})|(?<={),/, input, "")
-  end
-
-  def convert_to_lists(input) do
-    replaced_opening = Regex.replace(~r/{/, input, "[")
-    Regex.replace(~r/}/, replaced_opening, "]")
-  end
-
-  # Add up the groups
-  # Outermost group gets value of 1, next layer gets 2 and so on
-  def count_groups(group), do: count_groups(group, 1)
-  def count_groups(group, depth) do
-    # Own depth, plus the depth of all children
-    [depth] ++ Enum.map(group, &count_groups(&1, depth + 1))
-    |> Enum.sum
-  end
+  ## Handle garbage sections and cancle's
+  # When we hit a `!` ignore it and the next char
+  def garbage("!" <> <<_::bytes-size(1)>> <> rest, depth, count),
+    do: garbage(rest, depth, count)
+  # When we hit a `>` the garbage section is over, go back to normal parsing
+  def garbage(">" <> rest, depth, count), do: parse(rest, depth, count)
+  # When we hit anything while in a garbage section,
+  # ignore it and incremnt the garbage count
+  def garbage(<<_::bytes-size(1)>> <> rest, depth, count),
+    do: garbage(rest, depth, count + 1)
 
   def first_half(input) do
-    input
-    # Delete some stuff
-    |> delete_canceled
-    |> delete_garbage
-    |> delete_extra_commas
-
-    # Convert atoms to lists because they are enumerable
-    |> convert_to_lists
-
-    # Eval the string and get just the value
-    |> Code.eval_string
-    |> elem(0)
-
-    # Count up the groups
-    |> count_groups
+    elem(parse(input), 0)
   end
 
   def second_half(input) do
-    100
+    elem(parse(input), 1)
   end
 end
--- adventofcode/2017/elixir/test/puzzles/n09_test.exs
+++ adventofcode/2017/elixir/test/puzzles/n09_test.exs
@@ -14,53 +14,6 @@ defmodule AOCTest.Puzzles.N09Test do
     {:ok, input: input}
   end
 
-  describe "delete_canceled/1" do
-    test "delete some canceled chars" do
-      assert delete_canceled("{!<<>, {!}}}") == "{<>, {}}"
-    end
-
-    test "canceled cancel chars" do
-      assert delete_canceled("{{<!!>},{<!!>},{<!!>},{<!!>}}") ==
-        "{{<>},{<>},{<>},{<>}}"
-    end
-
-    test "no canceled chars" do
-      assert delete_canceled("{<>, {}}") == "{<>, {}}"
-    end
-  end
-
-  describe "delete_garbage/1" do
-    test "delete garbage sections" do
-      assert delete_garbage("{{<},{<},{<},{<a>}}") == "{{}}"
-    end
-  end
-
-  describe "delete_extra_commas/1" do
-    test "delete some extra commas" do
-      assert delete_extra_commas("{,,,}") == "{}"
-    end
-  end
-
-  describe "convert_to_lists" do
-    test "convert atoms to lists" do
-      assert convert_to_lists("{{},{},{{}}}") == "[[],[],[[]]]"
-    end
-  end
-
-  describe "count_groups/1" do
-    test "empty group" do
-      assert count_groups([]) == 1
-    end
-
-    test "(1 * 1) + (2 * 2) == 5" do
-      assert count_groups([[],[]]) == 5
-    end
-
-    test "(1 * 1) + (2 * 1) + (3 * 3) + (4 * 1)" do
-      assert count_groups([[[],[],[[]]]]) == 16
-    end
-  end
-
   describe "Part 1" do
     test "first example" do
       assert first_half("{{}}") == 3
@@ -72,20 +25,24 @@ defmodule AOCTest.Puzzles.N09Test do
 
     test "third example" do
       assert first_half("{<a>,<a>,<a>,<a>}") == 1
-    end
+    
 
     test "solution", %{:input => input} do
-      assert first_half(input) == 1044
+      assert first_half(input) == 8337
     end
   end
 
-  # describe "Part 2" do
-  #   test "" do
-  #     assert second_half() == 6
-  #   end
+  describe "Part 2" do
+    test "first example" do
+      assert second_half("{<random characters>}") == 17
+    end
 
-  #   test "solution", %{:input => input} do
-  #     assert second_half(input) == 1054
-  #   end
-  # end
+    test "second example" do
+      assert second_half(~s(<{o"i!a,<{i<a>)) == 10
+    end
+
+    test "solution", %{:input => input} do
+      assert second_half(input) == 4330
+    end
+  end
 end
