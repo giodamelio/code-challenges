b7010e79d1cda9fe9e731576c4970e9d0f74c148
Gio d'Amelio <giodamelio@gmail.com>
2018-09-23 13:51:24 -0700

    CryptoPals: Rust: init

---
+++ cryptopals/rust/shell.nix
@@ -0,0 +1,10 @@
+# { pkgs ? import <nixpkgs> {} }:
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  nixpkgs.mkShell {
+    buildInputs = [
+      nixpkgs.latest.rustChannels.nightly.rust
+    ];
+  }
