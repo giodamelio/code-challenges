2f0408e5c2d60fa4f0c72d76f44a90218a4fb239
Gio d'Amelio <giodamelio@gmail.com>
2018-09-23 23:21:11 -0700

    CryptoPals: Rust: complete set 1 challenge 1

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -5,8 +5,7 @@ mod hex;
 mod utils;
 
 fn main() {
-    let bytes = hex::string_to_bytes("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
-    println!("{:?}", bytes);
-
-    println!("{}", base64::encode(&[77, 97, 110]));
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let bytes = hex::string_to_bytes(input).expect("Failed to parse hex input");
+    println!("{}", base64::encode(&bytes));
 }
