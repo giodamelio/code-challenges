40192352633def5adeea60a2853d8bdcd9fddd72
Gio d'Amelio <giodamelio@gmail.com>
2018-09-23 17:48:55 -0700

    CryptoPals: Rust: improve hex to bytes function to use iterators

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -1,18 +1,13 @@
 #![feature(exact_chunks)]
 
 fn hex_string_to_bytes(input: &str) -> Vec<u8> {
-    let mut decoded_bytes = Vec::new();
-
     let bytes: Vec<_> = input.chars().collect();
 
-    for chunk in bytes.exact_chunks(2) {
+    bytes.exact_chunks(2).map(|chunk| {
         let chunk0 = chunk[0].to_digit(16).unwrap();
         let chunk1 = chunk[1].to_digit(16).unwrap();
-        let byte = ((chunk0 << 4) | chunk1) as u8;
-        decoded_bytes.push(byte);
-    }
-
-    decoded_bytes
+        ((chunk0 << 4) | chunk1) as u8
+    }).collect()
 }
 
 fn main() {
@@ -25,5 +20,6 @@ mod tests {
     #[test]
     fn simple_hex() {
         assert_eq!(hex_string_to_bytes("FFFF"), &[255, 255]);
+        assert_eq!(hex_string_to_bytes("0000"), &[0, 0]);
     }
 }
