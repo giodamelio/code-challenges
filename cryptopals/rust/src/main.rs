80463059f962f50f2f0f603aa5ad749708b49451
Gio d'Amelio <giodamelio@gmail.com>
2018-09-23 17:44:40 -0700

    CryptoPals: Rust: simple hex to bytes function

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -1,3 +1,29 @@
+#![feature(exact_chunks)]
+
+fn hex_string_to_bytes(input: &str) -> Vec<u8> {
+    let mut decoded_bytes = Vec::new();
+
+    let bytes: Vec<_> = input.chars().collect();
+
+    for chunk in bytes.exact_chunks(2) {
+        let chunk0 = chunk[0].to_digit(16).unwrap();
+        let chunk1 = chunk[1].to_digit(16).unwrap();
+        let byte = ((chunk0 << 4) | chunk1) as u8;
+        decoded_bytes.push(byte);
+    }
+
+    decoded_bytes
+}
+
 fn main() {
-    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_hex() {
+        assert_eq!(hex_string_to_bytes("FFFF"), &[255, 255]);
+    }
 }
