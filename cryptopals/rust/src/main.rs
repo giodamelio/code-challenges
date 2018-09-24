b034721877975dc284f9d02bafc2a6b52235dc0a
Gio d'Amelio <giodamelio@gmail.com>
2018-09-23 18:20:03 -0700

    CryptoPals: Rust: improve hex to bytes function to handle errors with a result

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -1,12 +1,34 @@
 #![feature(exact_chunks)]
 
-fn hex_string_to_bytes(input: &str) -> Vec<u8> {
+use std::error;
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq)]
+struct InvalidHexCharError;
+
+impl fmt::Display for InvalidHexCharError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "not a valid hex character")
+    }
+}
+
+impl error::Error for InvalidHexCharError {
+    fn description(&self) -> &str {
+        "not a valid hex char"
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+}
+
+fn hex_string_to_bytes(input: &str) -> Result<Vec<u8>, InvalidHexCharError> {
     let bytes: Vec<_> = input.chars().collect();
 
     bytes.exact_chunks(2).map(|chunk| {
-        let chunk0 = chunk[0].to_digit(16).unwrap();
-        let chunk1 = chunk[1].to_digit(16).unwrap();
-        ((chunk0 << 4) | chunk1) as u8
+        let chunk0 = chunk[0].to_digit(16).ok_or(InvalidHexCharError)?;
+        let chunk1 = chunk[1].to_digit(16).ok_or(InvalidHexCharError)?;
+        Ok(((chunk0 << 4) | chunk1) as u8)
     }).collect()
 }
 
@@ -19,7 +41,12 @@ mod tests {
 
     #[test]
     fn simple_hex() {
-        assert_eq!(hex_string_to_bytes("FFFF"), &[255, 255]);
-        assert_eq!(hex_string_to_bytes("0000"), &[0, 0]);
+        assert_eq!(hex_string_to_bytes("FFFF").unwrap(), &[255, 255]);
+        assert_eq!(hex_string_to_bytes("0000").unwrap(), &[0, 0]);
+    }
+
+    #[test]
+    fn invalid_hex() {
+        assert_eq!(hex_string_to_bytes("ZZZZ").unwrap_err(), InvalidHexCharError)
     }
 }
