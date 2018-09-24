cdc29465fe8eafaace1cd1638879b06ef998a68e
Gio d'Amelio <giodamelio@gmail.com>
2018-09-23 18:33:45 -0700

    CryptoPals: Rust: improve hex to bytes function to include invalid char in error

--- cryptopals/rust/src/main.rs
+++ cryptopals/rust/src/main.rs
@@ -4,11 +4,11 @@ use std::error;
 use std::fmt;
 
 #[derive(Debug, Clone, PartialEq)]
-struct InvalidHexCharError;
+struct InvalidHexCharError(char);
 
 impl fmt::Display for InvalidHexCharError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "not a valid hex character")
+        write!(f, "'{}' is not a valid hex character", self.0)
     }
 }
 
@@ -26,8 +26,8 @@ fn hex_string_to_bytes(input: &str) -> Result<Vec<u8>, InvalidHexCharError> {
     let bytes: Vec<_> = input.chars().collect();
 
     bytes.exact_chunks(2).map(|chunk| {
-        let chunk0 = chunk[0].to_digit(16).ok_or(InvalidHexCharError)?;
-        let chunk1 = chunk[1].to_digit(16).ok_or(InvalidHexCharError)?;
+        let chunk0 = chunk[0].to_digit(16).ok_or(InvalidHexCharError(chunk[0]))?;
+        let chunk1 = chunk[1].to_digit(16).ok_or(InvalidHexCharError(chunk[1]))?;
         Ok(((chunk0 << 4) | chunk1) as u8)
     }).collect()
 }
@@ -47,6 +47,6 @@ mod tests {
 
     #[test]
     fn invalid_hex() {
-        assert_eq!(hex_string_to_bytes("ZZZZ").unwrap_err(), InvalidHexCharError)
+        assert_eq!(hex_string_to_bytes("ZZZZ").unwrap_err(), InvalidHexCharError('Z'))
     }
 }
