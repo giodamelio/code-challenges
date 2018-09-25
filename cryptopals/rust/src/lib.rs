2c9a25f67d6ed63a97242df7fc696cb779d60803
Gio d'Amelio <giodamelio@gmail.com>
2018-09-24 20:43:16 -0700

    CryptoPals: Rust: convert project from cli to library

---
+++ cryptopals/rust/src/lib.rs
@@ -0,0 +1,20 @@
+#![feature(exact_chunks)]
+
+pub mod base64;
+pub mod hex;
+pub mod utils;
+
+#[cfg(test)]
+mod set1 {
+    use super::*;
+
+    #[test]
+    fn problem1() {
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let bytes = hex::string_to_bytes(input).expect("Failed to parse hex input");
+        assert_eq!(
+            base64::encode(&bytes),
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
+    }
+}
