3c7b24ee0aa32f27a3a89f0bbe2550c9dab16090
Gio d'Amelio <giodamelio@gmail.com>
2018-09-24 22:05:56 -0700

    CryptoPals: Rust: add hex encode

--- cryptopals/rust/src/hex.rs
+++ cryptopals/rust/src/hex.rs
@@ -1,3 +1,4 @@
+use std::char;
 use std::error;
 use std::fmt;
 
@@ -51,28 +52,60 @@ pub fn decode(input: &str) -> Result<Vec<u8>, HexToBytesError> {
         }).collect()
 }
 
+pub fn encode(input: &[u8]) -> String {
+    input
+        .iter()
+        .map(|digit| {
+            vec![
+                char::from_digit(u32::from((*digit >> 4) & 0b0000_1111), 16),
+                char::from_digit(u32::from(*digit & 0b0000_1111), 16),
+            ]
+        }).flatten()
+        // c will always be Some(char) since we can never have an input larget then one byte large
+        .map(|c| c.unwrap())
+        .collect::<String>()
+        .to_uppercase()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn simple_hex() {
-        assert_eq!(decode("FFFF").unwrap(), &[255, 255]);
-        assert_eq!(decode("0000").unwrap(), &[0, 0]);
+    mod decode {
+        use super::*;
+
+        #[test]
+        fn simple_hex() {
+            assert_eq!(decode("FFFF").unwrap(), &[255, 255]);
+            assert_eq!(decode("0000").unwrap(), &[0, 0]);
+        }
+
+        #[test]
+        fn invalid_hex() {
+            assert_eq!(
+                decode("ZZZZ").unwrap_err(),
+                HexToBytesError::InvalidChar('Z')
+            )
+        }
+
+        #[test]
+        fn not_in_pairs() {
+            assert_eq!(decode("F").unwrap_err(), HexToBytesError::InvalidLength);
+            assert_eq!(decode("FFF").unwrap_err(), HexToBytesError::InvalidLength);
+            assert_eq!(decode("").unwrap_err(), HexToBytesError::InvalidLength);
+        }
     }
 
-    #[test]
-    fn invalid_hex() {
-        assert_eq!(
-            decode("ZZZZ").unwrap_err(),
-            HexToBytesError::InvalidChar('Z')
-        )
-    }
+    mod encode {
+        use super::*;
 
-    #[test]
-    fn not_in_pairs() {
-        assert_eq!(decode("F").unwrap_err(), HexToBytesError::InvalidLength);
-        assert_eq!(decode("FFF").unwrap_err(), HexToBytesError::InvalidLength);
-        assert_eq!(decode("").unwrap_err(), HexToBytesError::InvalidLength);
+        #[test]
+        fn simple_hex() {
+            assert_eq!(
+                encode(&[255, 127, 63, 31, 15, 7, 3, 1, 0]),
+                "FF7F3F1F0F07030100"
+            );
+            assert_eq!(encode(&[10, 23]), "0A17");
+        }
     }
 }
