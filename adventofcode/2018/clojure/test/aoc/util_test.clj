06cc2766c0572ed905df8ec9530a62ac0f7724f5
Gio d'Amelio <giodamelio@gmail.com>
2018-12-02 21:05:48 -0800

    AOC: Clojure: add util functions to help with regex and named capture groups

--- adventofcode/2018/clojure/test/aoc/util_test.clj
+++ adventofcode/2018/clojure/test/aoc/util_test.clj
@@ -6,3 +6,18 @@
        (parse-int "10") => 10
        (parse-int "+10") => 10
        (parse-int "-10") => -10)
+
+(fact "get-capture-groups"
+      (let [get-capture-groups #'aoc.util/get-capture-groups]
+        (get-capture-groups #"(?<letter>[a-z]+) (?<number>\d+)") =>
+        '("letter" "number")
+        (get-capture-groups #"") =>
+        '()))
+
+(fact "re-find-named-groups"
+      (re-find-named-groups #"(?<letter>[a-z]+) (?<number>\d+)" "abc 22") =>
+      {:letter "abc"
+       :number "22"}
+      (re-find-named-groups #"([a-z]+) (\d+)" "abc 22") => {}
+      (re-find-named-groups #"[a-z]+ \d+" "abc 22") => {}
+      (re-find-named-groups #"^\d+$" "abc") => {})
