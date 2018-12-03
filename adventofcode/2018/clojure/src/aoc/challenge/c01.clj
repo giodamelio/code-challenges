1fddd3ce7175e980f9b8935fbaeee800865659a0
Gio d'Amelio <giodamelio@gmail.com>
2018-12-02 17:17:35 -0800

    AOC: Clojure: day 1

--- adventofcode/2018/clojure/src/aoc/challenge/c01.clj
+++ adventofcode/2018/clojure/src/aoc/challenge/c01.clj
@@ -1,4 +1,22 @@
-(ns aoc.challenge.c01)
+(ns aoc.challenge.c01
+  (:require [clojure.string :as str]
+            [aoc.util :refer :all]))
+
+(defn parse-input [input]
+  (->> input
+       (str/split-lines)
+       (map parse-int)))
+
+(defn cycled-freqs [input]
+  (reductions + (cycle input)))
 
 (defn answer-part-1 [input]
-  10)
+  (reduce + (parse-input input)))
+
+(defn answer-part-2 [input]
+  (loop [freqs (cycled-freqs (parse-input input))
+         freqs-visited #{}]
+    (let [current-freq (first freqs)]
+      (if (contains? freqs-visited current-freq)
+        current-freq
+        (recur (rest freqs) (conj freqs-visited current-freq))))))
--- adventofcode/2018/clojure/test/aoc/challenge/c01_test.clj
+++ adventofcode/2018/clojure/test/aoc/challenge/c01_test.clj
@@ -3,5 +3,18 @@
             [aoc.challenge.c01 :refer :all]
             [aoc.util :refer [load-input]]))
 
-(fact "answer-part-1"
-      (answer-part-1 (load-input)) => 10)
+(def example-input "+1\n-2\n+3\n+1")
+
+(fact "parse-input"
+       (parse-input "+10\n-20\n+22222") => [10 -20 22222])
+
+(fact "cycled-freqs"
+      (take 5 (cycled-freqs [1 -1])) => [1 0 1 0 1])
+
+(facts "answer-part-1"
+      (answer-part-1 example-input) => 3
+      (answer-part-1 (load-input)) => 497)
+
+(facts "answer-part-2"
+      (answer-part-2 example-input) => 2
+      (answer-part-2 (load-input)) => 558)
