249c451778f4b0d1fa1218e79198ae331819ffab
Gio d'Amelio <giodamelio@gmail.com>
2020-08-06 20:56:02 -0700

    AOC_2019: Csharp: Day 1 part one complete

--- adventofcode/2019/csharp/advent_of_code/Program.cs
+++ adventofcode/2019/csharp/advent_of_code/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace advent_of_code
 {
@@ -6,27 +7,53 @@ namespace advent_of_code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var exampleInput = new List<int> {100756};
+            var day1 = new Day1(exampleInput);
+            Console.WriteLine(day1.Part1());
         }
     }
 
     class Solution<T>
     {
-        private T _input;
+        protected readonly T Input;
 
-        public Solution(T input)
+        protected Solution(T input)
         {
-            this._input = input;
+            this.Input = input;
         }
 
         public int Part1()
         {
-            
+            return 0;
         }
 
         public int Part2()
         {
-            
+            return 0;
+        }
+    }
+
+    class Day1 : Solution<List<int>>
+    {
+        public Day1(List<int> input) : base(input)
+        {
+        }
+
+        public new int Part1()
+        {
+            var totalMass = 0;
+
+            foreach (var moduleMass in Input)
+            {
+                totalMass += CalculateRequiredFuel(moduleMass);
+            }
+
+            return totalMass;
+        }
+
+        private static int CalculateRequiredFuel(int mass)
+        {
+            return (mass / 3) - 2;
         }
     }
 }
